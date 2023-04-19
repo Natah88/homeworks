2acec27e384e4646c429e60b5d7ac2ed5179660a
Natalia_Shishkina <natah88@mail.ru>
2023-04-19 14:03:12 +0700

    task_1.1 DONE

--- lvl_1/task_1.1.py
+++ lvl_1/task_1.1.py
@@ -6,3 +6,7 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+print(my_favorite_songs[:13])
+print(my_favorite_songs[64:77])
+print(my_favorite_songs[16:30])
+print(my_favorite_songs[51:62])
