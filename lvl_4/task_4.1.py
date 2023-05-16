2bf174204e60ebe5d131b67421479d5257ab4c53
Natalia_Shishkina <natah88@mail.ru>
2023-05-16 13:17:17 +0700

    task_4.1 DONE

--- lvl_4/task_4.1.py
+++ lvl_4/task_4.1.py
@@ -20,3 +20,58 @@
 # Имя студента:
 # ID школы:
 # Название школы:
+
+import sqlite3
+# 1
+# connection = sqlite3.connect(r'C:\Users\natah\envs\homeworks\lvl_4\teatchers.db') 
+# cursor = connection.cursor()                
+# sqlquery = """CREATE TABLE Students (
+# Student_Id INTEGER,
+# Student_Name TEXT, 
+# School_Id INTEGER NOT NULL PRIMARY KEY);"""
+# cursor.execute(sqlquery)                   
+# connection.commit()              
+# connection.close() 
+
+# 2
+# connection = sqlite3.connect(r'C:\Users\natah\envs\homeworks\lvl_4\teatchers.db') 
+# cursor = connection.cursor()                
+# sqlquery = """INSERT INTO Students (Student_Id, Student_Name, School_Id)
+# VALUES
+# (201, 'Иван', 1),
+# (202, 'Петр', 2),
+# (203, 'Анастасия', 3),
+# (204, 'Игорь', 4);"""
+# cursor.execute(sqlquery)                    
+# connection.commit()                
+# connection.close()
+
+# 3
+def get_connection():
+  connection = sqlite3.connect(r'C:\Users\natah\envs\homeworks\lvl_4\teatchers.db')
+  return connection
+
+def close_connection(connection):
+  if connection:
+    connection.close()
+
+def get_student_info(Student_Id):
+  try:
+    connection = get_connection()
+    cursor = connection.cursor()
+    select_query = """SELECT Students.Student_Id, Students.student_name, School.School_Id, School.School_Name
+From Students
+JOIN School ON Students.School_Id = School.School_Id
+WHERE Student_Id = ?;"""
+    cursor.execute(select_query, (Student_Id,))
+    records = cursor.fetchall()
+    close_connection(connection)
+    for row in records:
+      print("ID Студента:", row[0])
+      print("Имя студента:", row[1])
+      print("ID школы:", row[2])
+      print("Название школы:", row[3])
+  except (Exception, sqlite3.Error) as error:
+    print("Ошибка в получении данных: ", error) 
+
+get_student_info(201)
